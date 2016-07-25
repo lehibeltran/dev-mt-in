a0f28825be0c64ecb7aee540236ec1b69621d118
lehibeltran86 <lehibeltran@hotmail.com>
2016-07-24 20:40:16 -0600

    update modal and configured db.json

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 var express = require('express');
 var config = require('./config.js');
+var jsonServer = require('json-server');
+var databaseServer = jsonServer.create();
+var router = jsonServer.router('db.json');
+var middlewares = jsonServer.defaults();
+
 var port = config.portNum;
 var app = express();
-
 app.use(express.static('public'));
 app.use('/packages', express.static(__dirname + '/node_modules/'));
 
 app.listen(port, function() {
     console.log('Making tacos at: ', port);
 });
+
+databaseServer.use(middlewares);
+databaseServer.use(router);
+databaseServer.listen(3000);
